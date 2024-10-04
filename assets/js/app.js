7a5d4f178f25701e479b9ecf80bbd0f46633b5e8
Fwzryy <raihan0603@gmail.com>
2024-10-04 16:06:38 +0700

    Current weather section

--- assets/js/app.js
+++ assets/js/app.js
@@ -54,7 +54,7 @@ searchField.addEventListener("input", function() {
           `;
 
           const /** {NodeList} \ [] */ items =[];
-
+          
           for (const { name, lat, lon, country, state } of locations) {
             const searchItem = document.createElement("li");
             searchItem.classList.add("view-item");
@@ -74,12 +74,15 @@ searchField.addEventListener("input", function() {
             searchResult.querySelector("[data-search-list]").appendChild(searchItem);
             items.push(searchItem.querySelector("[data-search-toggler]"));
           }
+          addEventOnElements(items, "click", function(){
+            toggleSearch();
+            searchResult.classList.remove("active");
+          })
         });
     }, searchTimeoutDuration);
   }
 
 });
-
 
 const container = document.querySelector("[data-container]");
 const loading = document.querySelector("[data-loading]");
@@ -92,9 +95,9 @@ const errorContent = document.querySelector("[data-error-content]");
  */
 export const updateWeather = function (lat, lon) {
     
-    loading.style.display = "grid";
+    // loading.style.display = "grid";
     container.style.overflowY = "hidden";
-    container.classList.contains("fade-in") ?? container.classList.remove("fade-in");
+    // container.classList.remove("fade-in");
     errorContent.style.display = "none";
 
     const currentWeatherSection = document.querySelector("[data-current-weather]");
@@ -113,4 +116,56 @@ export const updateWeather = function (lat, lon) {
       currentLocationBtn.removeAttribute("disabled");
     }
 
+    /**
+      CURRENT WEATHER SECTION
+     */
+      fetchData(url.currentWeather(lat, lon), function(currentWeather){
+        
+        const {
+          weather,
+          dt:dateUnix,
+          sys: { sunrise: sunriseUnixUTC, sunset: sunsetUnixUTC },
+          main: { temp, feels_like, pressure, humidity },
+          visibility, 
+          timezone
+        } = currentWeather
+        const [{ description, icon }] = weather;
+
+        const card = document.createElement("div");
+        card.classList.add("card", "card-lg", "current-weather-card");
+
+        card.innerHTML = `
+            <h2 class="title-2 card-title">Now</h2>
+
+                <div class="wrapper">
+                  <p class="heading">${parseInt(temp)}&deg;<sup>c</sup></p>
+
+                  <img src="./assets/images/weather_icons/${icon}.png" width="64"
+                  height="64" alt="${description}" 
+                  class="weather-icon">
+                </div>
+
+                  <p class="body-3">${description}</p>
+
+                <ul class="meta-list">
+                  <li class="meta-item">
+                    <span class="m-icon">calendar_today</span>
+                    <p class="title-3 meta-text">${module.getDate(dateUnix, timezone)}</p>
+                  </li>
+
+                  <li class="meta-item">
+                    <span class="m-icon">location_on</span>
+                    <p class="title-3 meta-text" data-location></p>
+                  </li>
+                </ul>
+              `;
+        
+        fetchData(url.reverseGeo(lat, lon), function([{ name, country }]) {
+          card.querySelector("[data-location]").innerHTML = `${name}, ${country}`
+        });
+        currentWeatherSection.appendChild(card)
+      });
+
 }
+
+export const error404 = function () {}
